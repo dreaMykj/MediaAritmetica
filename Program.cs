d5ed83e8a56d7a45c07acbebe095a069b9834d1e
dreaMykj <gabrielgreghi2018@gmail.com>
2023-10-18 11:41:17 -0300

    Concluido

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿double primeiro, segundo, terceiro, media;
+
+Console.WriteLine("Média Aritmética de 3 números");
+
+Console.Write("Digite o primeiro número: ");
+primeiro = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o segundo número: ");
+segundo = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o terceiro número: ");
+terceiro = Convert.ToDouble(Console.ReadLine());
+
+media = (primeiro + segundo + terceiro) / 3;
+Console.WriteLine($"Média: {media:N1}");
